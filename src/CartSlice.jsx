c90d458c017ba80bbfb677f6ddea4bfe3213e701
Tristan Andersen <tpan02@pm.me>
2024-10-25 00:09:34 +0000

    Create cart reducers for adding, removing and updating an items quantity

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      // extract item's info from action.payload
+      // look for item in state.items to match extracted name
+      // if item exists then add 1 to quantity, otherwise add item to cart with 1 quantity
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({name, image, cost, quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      // filter out items that matches the name from action.payload
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      // extract item's name and amount from action.payload
+      // look for item in state.items to match extracted name
+      // update item's quantity to new amount provided in payload
+      const {name, quantity} = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
